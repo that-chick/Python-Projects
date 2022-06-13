0c6a81594460a5b3a2bd61785f5efa66bc1532c2
Alison Michelle Wing <102198807+that-chick@users.noreply.github.com>
2022-06-13 15:57:27 -0600

    Create Initials.script.py

---
+++ Initials.script.py
@@ -0,0 +1,12 @@
+name ="""
+   A     W   W
+  A A    W   W
+ A   A   W   W
+ AAAAA   W W W
+A     A  WW WW
+A     A  WW WW
+A     A  W   W
+"""
+print (name)
+ # Alison Wing
+ # I'm a realtor/mom - boss woman 
