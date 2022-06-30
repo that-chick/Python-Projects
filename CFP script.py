882d2497a068d65a341560182e383926596eb0a9
Alison Michelle Wing <102198807+that-chick@users.noreply.github.com>
2022-06-30 14:11:21 -0600

    Control Flow (beginner): Space Hub Project

--- CFP script.py
+++ CFP script.py
@@ -1,5 +1,3 @@
-#spacehubfun
-
 print("I have information for the following planets:\n")
 
 print("   1. Venus   2. Mars    3. Jupiter")
@@ -8,7 +6,6 @@ print("   4. Saturn  5. Uranus  6. Neptune\n")
 weight = 185
 planet = 3
 
-# Write an if statement below:
 if planet == 1:
   weight = weight * 0.91
 elif planet == 2:
