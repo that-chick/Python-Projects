65b984100c9dafa630a79c830a51bbe3e04ef1b8
Alison Michelle Wing <102198807+that-chick@users.noreply.github.com>
2022-07-01 18:12:36 -0600

    Control Flow Challenge 4: Advanced

---
+++ CFC4A: script.py
@@ -0,0 +1,13 @@
+def movie_review(rating):
+  if (rating <= 5):
+    return "Avoid at all costs!"
+  if (rating < 9):
+    return "This one was fun."
+  return "Outstanding!"
+
+
+print(movie_review(9))
+
+print(movie_review(4))
+
+print(movie_review(6))
